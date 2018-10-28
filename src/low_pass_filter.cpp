ad880b9a21c0f33a68f85e3ca1377e7c560078c2
k-ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2018-10-28 01:08:07 +0000

    Fix indent

--- src/low_pass_filter.cpp
+++ src/low_pass_filter.cpp
@@ -8,11 +8,11 @@
 #define A0 (1.0 + ALPHA)
 
 LowPassFilter::LowPassFilter(float cutOffFreq, float q)
-        : a1((-2.0 * cos(OMEGA)) / A0), a2((1.0 - ALPHA) / A0),
-              b0((1.0 - cos(OMEGA)) / 2.0 / A0), b1((1.0 - cos(OMEGA)) / A0) {
-                  out1 = out2 = 0.0;
-                  in1 = in2 = 0.0;
-              }
+    : a1((-2.0 * cos(OMEGA)) / A0), a2((1.0 - ALPHA) / A0),
+    b0((1.0 - cos(OMEGA)) / 2.0 / A0), b1((1.0 - cos(OMEGA)) / A0) {
+        out1 = out2 = 0.0;
+        in1 = in2 = 0.0;
+    }
 
 float LowPassFilter::apply(float input) {
     float output = b0 * input + b1 * in1 + b0 * in2 - a1 * out1 - a2 * out2;
