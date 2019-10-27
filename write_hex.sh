563f799000515f572b3db0f7952b660b15642b85
Kazuki Ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2019-10-27 06:09:17 +0000

    Refactor write_hex.sh

--- write_hex.sh
+++ write_hex.sh
@@ -4,5 +4,4 @@ ARDUINO_TOOLS=/Applications/Arduino.app/Contents/Java/hardware/tools
 HEX_FILE=.pio/build/default/firmware.hex
 TARGET=atmega328pb
 
-# ${ARDUINO_TOOLS}/avr/bin/avrdude -C ${ARDUINO_TOOLS}/avr/etc/avrdude.conf -b 115200 -c arduino -P $1 -p ${TARGET} -U flash:w:${HEX_FILE}:i
-${ARDUINO_TOOLS}/avr/bin/avrdude -C ${ARDUINO_TOOLS}/avr/etc/avrdude.conf -b 115200 -c atmelice_isp -p atmega328pb -U flash:w:${HEX_FILE}:i
+${ARDUINO_TOOLS}/avr/bin/avrdude -C ${ARDUINO_TOOLS}/avr/etc/avrdude.conf -b 115200 -c atmelice_isp -p ${TARGET} -U flash:w:${HEX_FILE}:i
